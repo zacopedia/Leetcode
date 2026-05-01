44a2fe4d36383da193b01e01abc7d2495f163fd9
zac <abderrazaklahcene90@gmail.com>
2026-05-01 18:29:12 +0100

    push changes

--- main.cpp
+++ main.cpp
@@ -815,6 +815,26 @@ void countDistinct () {
 // 8- Ask the user to enter 5 numbers
 // Find the first number that repeats
 
+void findFirstRepeat () {
+    cout << "Enter 5 Numbers\n";
+    int arr[5];
+
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (arr[i] == arr[j]) {
+                cout << arr[i] << endl;
+                return;
+            }
+        }
+    }
+
+    cout << "No Repeating Number" << endl;
+}
+
 // ///////////////////////////////////////////////
 
 // 9- Ask the user to enter 5 numbers
@@ -881,7 +901,8 @@ int main () {
     // countAppearNum ();
     // countPairs ();
     // findNumAppear2Times ();
-    countDistinct ();
+    // countDistinct ();
+    findFirstRepeat ();
     return 0;
 }
 
